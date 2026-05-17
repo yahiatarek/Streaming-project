f8f9f2b56320b736dec900658ca62e1515931de3
yahia.tarek <yahia.tarek@vodafone.com>
2026-05-17 19:38:19 +0300

    upload service init

---
+++ upload-service/src/test/java/com/upload/upload_service/UploadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upload.upload_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
